ea49d39e30242ddd85edb1d1ff5ff1a441e05e03
MrXanter <orimakc2@gmail.com>
2025-06-03 18:08:03 +0200

    pictures are saved as bytes.

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,12 +1,13 @@
 from typing import Annotated
 from aiogram import Router, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, BufferedInputFile
+from aiogram.filters import Command, StateFilter
 import asyncio
 import bot.keyboard as kb
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-
+from PIL import Image
+from io import BytesIO
 
 router = Router()
 
@@ -32,10 +33,30 @@ async def handle_photo(message: PhotoMessage, state: FSMContext):
                         reply_markup=kb.back_button)
 
 
-@router.message(PhotoState.waiting_for_photo)
+@router.message(F.photo, PhotoState.waiting_for_photo)
 async def handle_photo_upload(message: Message, state: FSMContext):
-        file_id = message.photo[-1].file_id
-        await message.answer_photo(file_id, caption="Here is your image!")
+        picture = message.photo[-1]
+        file = await message.bot.get_file(picture.file_id)
+        file_path = file.file_path
+
+        photo_bytes = await message.bot.download_file(file_path)
+        image = Image.open(BytesIO(photo_bytes.read()))
+
+        image = image.convert("RGB")
+        image = image.resize((800, 800))
+
+        output_buffer = BytesIO()
+        image.save(output_buffer, format="JPEG")
+        output_buffer.seek(0)
+
+        await message.answer_photo(
+            photo=BufferedInputFile(
+                file=output_buffer.read(),
+                filename="restyled.jpg"
+            ),
+            caption="Your image style has been changed!",
+        )
+
         
         
         await state.clear()
@@ -43,7 +64,8 @@ async def handle_photo_upload(message: Message, state: FSMContext):
                             reply_markup=kb.buttons)
 
 
-@router.message(Command("back"))
-async def handle_back(message: Message):
+@router.message(Command("back"), StateFilter("*"))
+async def handle_back_command(message: Message, state: FSMContext):
+    await state.clear()
     await message.answer("Back to main.",
                         reply_markup=kb.buttons)
